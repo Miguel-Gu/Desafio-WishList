c94c86d5ca36bcf8a55897878e1da7d0a670e7eb
Gustavo Keiji <gugu.keiji.2004@gmail.com>
2021-10-26 15:18:10 -0300

    Banco criado

---
+++ BD_Scripts/Wish_List_DML.sql
@@ -0,0 +1,10 @@
+USE Wish_List;
+GO
+
+INSERT INTO USUARIOS (email, senha)
+VALUES ('user@gmail.com','user123')
+GO
+
+INSERT INTO DESEJOS (idUsuarios, nomeDesejo, descricao)
+VALUES ('1','Pular de Paraquedas','Pular de um avião com um paraquedas nas costas')
+GO
